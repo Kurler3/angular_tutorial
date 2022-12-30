a61a11137ff0359c3b9b16f83b7820d96d60444a
Kurler3 <miguelbento2000@gmail.com>
2022-12-30 16:27:56 +0000

    Working on it :d

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  title:string = 'Task Tracker';
 
+  constructor() {}
+
+  // LIFE CYCLE METHOD (GOOD FOR FETCHING DATA)
+  ngOnInit(): void {
+      
+  }
+
+  toggleAddTask() {
+    console.log('Add...');
+  }
 }
